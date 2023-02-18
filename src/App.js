b2dd66e479b4c624bf1e9ec9ff92fb3a11f73aeb
alextelfer <33209781+alextelfer@users.noreply.github.com>
2023-02-18 16:08:44 -0700

    changing website title

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
 import './App.css';
+import About from './pages/About';
+import EventInfo from './pages/EventInfo';
+import HomePage from './pages/Home';
+import RootLayout from './pages/Root';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <RootLayout />,
+    children: [
+      { index: true, element: <HomePage />},
+      { path: '/event', element: <EventInfo />},
+      { path: '/about', element: <About />}
+    ]
+  },
+]);
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <RouterProvider router={router}/>;
+    
 }
 
 export default App;
